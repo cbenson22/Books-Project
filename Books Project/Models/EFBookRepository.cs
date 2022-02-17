ecca1615854d57f93c3d9a4b4832c55340baedb1
cbenson22 <colepbenson22@gmail.com>
2022-02-16 21:08:51 -0700

    Add project files.

---
+++ Books Project/Models/EFBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books_Project.Models
+{
+    public class EFBookRepository : IBooksRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
---
+++ Books Project/Models/IBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books_Project.Models
+{
+    public interface IBooksRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
